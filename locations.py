091a6d71af1babf3592e6376cd7c33a4437333cb
Aarav Gupta <aaravgupta@Aaravs-MacBook-Pro-221.local>
2025-09-20 15:05:45 -0500

    locations 2

---
+++ locations.py
@@ -0,0 +1,12 @@
+serveries = {
+
+    "Seibel Servery": (29.716055103624868, -95.3984237601105),  
+    "South Servery": (29.715216425851025, -95.4012140739362), 
+    "Baker Servery": (29.7174, -95.4018),
+    "North Servery": (29.7180, -95.4000),
+    "West Servery": (29.7165, -95.4030),  
+}
+
+rice_village_restaurants = {
+    
+}
