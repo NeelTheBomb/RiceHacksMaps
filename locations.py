473a824b44d8d75e3f4e48d7429d88d3811462df
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2025-09-20 16:21:29 -0500

    better coordinates

--- locations.py
+++ locations.py
@@ -2,11 +2,14 @@ serveries = {
 
     "Seibel Servery": (29.716055103624868, -95.3984237601105),  
     "South Servery": (29.715216425851025, -95.4012140739362), 
-    "Baker Servery": (29.7174, -95.4018),
-    "North Servery": (29.7180, -95.4000),
-    "West Servery": (29.7165, -95.4030),  
+    "Baker Servery": (29.71707108412888, -95.39941726255175),
+    "North Servery": (29.721759712286712, -95.3978340093412),
+    "West Servery": (29.72105476132729, -95.39853215484233),  
 }
 
 rice_village_restaurants = {
     
 }
+
+
+#delivery_price = (distance_from_buyer_to_servery) * 8
